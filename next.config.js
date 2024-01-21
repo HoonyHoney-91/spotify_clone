a20e5f1f7daafa451b6209087785ea38d1393fbd
HoonyHoney-91 <shinx333@morris.umn.edu>
2024-01-21 01:58:39 -0800

    final

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    images: {
+        domains:[
+            "ukzxcbxcozricfupecjc.supabase.co"
+        ]
+    }
+}
 
 module.exports = nextConfig
